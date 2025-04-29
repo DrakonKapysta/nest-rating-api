a2ee406cb2ee141fe9bb4ec94f49b5743a382dfe
Oleg Matyokin <matyiokin2002@gmail.com>
2025-04-29 20:07:49 +0300

    feat: added empty controllers and created basic dtos

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductController } from './product.controller';
 
 @Module({
 	imports: [],
 	providers: [],
-	controllers: [],
+	controllers: [ProductController],
 	exports: [],
 })
 export class ProductModule {}
