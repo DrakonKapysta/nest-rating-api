79b91fae2595bd153daec9c92cd8f7f28d72740a
Oleg Matyokin <matyiokin2002@gmail.com>
2025-04-30 19:47:24 +0300

    feature: conneceted to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-	imports: [ProductModule, AuthModule, ReviewModule, TopPageModule],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
+		ProductModule,
+		AuthModule,
+		ReviewModule,
+		TopPageModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
